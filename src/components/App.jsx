68929b9ba72c4c4cf5844d8285f9af197a40d7a4
Serhii Sydorenko <sidorchik91@gmail.com>
2023-03-18 22:46:25 +0200

    moving fetch logic to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,74 @@
 import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
+import { fetchImages } from '../services/images-api';
+import Button from './Button';
+import Loader from './Loader';
 
 export class App extends Component {
   state = {
     searchQuery: '',
+    page: 1,
+    searchResult: [],
+    isLoading: false,
+    error: null,
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    const { searchQuery, page } = this.state;
+    if (prevState.searchQuery !== searchQuery) {
+      this.fetchHandler();
+    }
+    if (
+      prevState.page !== page &&
+      prevState.searchQuery === this.state.searchQuery
+    ) {
+      this.fetchHandler();
+    }
+  }
+
   handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery: searchQuery });
+    this.setState({ searchQuery, page: 1, searchResult: [] });
+  };
+
+  fetchHandler = () => {
+    const { searchQuery, page } = this.state;
+    this.setState({ isLoading: true });
+    setTimeout(() => {
+      fetchImages(searchQuery, page)
+        .then(result => {
+          if (result.hits.length === 0) {
+            this.setState({ searchResult: [], page: 1 });
+            return toast(
+              `There are no images by search request "${this.props.searchQuery}"`,
+              { theme: 'dark' }
+            );
+          }
+          this.setState(prevState => ({
+            searchResult: [...prevState.searchResult, ...result.hits],
+          }));
+        })
+        .catch(error => this.setState({ error }))
+        .finally(this.setState({ isLoading: false }));
+    }, 500);
+  };
+
+  onLoadMoreClick = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   render() {
-    const { searchQuery } = this.state;
+    const { isLoading, searchResult } = this.state;
     return (
       <div className="app">
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery searchQuery={searchQuery} />
+        {searchResult.length > 0 && (
+          <ImageGallery searchResult={searchResult} />
+        )}
+        {isLoading && <Loader />}
+        {searchResult.length > 0 && <Button onClick={this.onLoadMoreClick} />}
         <ToastContainer theme="dark" />
       </div>
     );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,69 +1,13 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { toast } from 'react-toastify';
-import { fetchImages } from '../../services/images-api';
 import Modal from '../Modal';
 import css from './ImageGallery.module.css';
 import GalleryItem from '../GalleryItem';
-import Button from '../Button';
-import Loader from '../Loader';
 
 class ImageGallery extends Component {
   state = {
-    searchQuery: '',
-    searchResult: [],
-    page: 0,
-    isLoading: false,
     showModal: false,
     largeImageURL: '',
-    error: null,
-  };
-
-  componentDidMount() {
-    this.fetchHandler();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { searchQuery } = this.props;
-    const { page } = this.state;
-    if (prevProps.searchQuery !== searchQuery) {
-      this.setState({
-        searchResult: [],
-        page: 1,
-        searchQuery: searchQuery,
-      });
-    }
-      if (prevState.page !== page) {
-      this.fetchHandler();
-    }
-  }
-
-  fetchHandler = () => {
-    if (this.state.page === 0) {
-      return;
-    }
-    this.setState({ isLoading: true });
-    setTimeout(() => {
-      fetchImages(this.props.searchQuery, this.state.page)
-        .then(result => {
-          if (result.hits.length === 0) {
-            this.setState({ searchResult: [], page: 1 });
-            return toast(
-              `There are no images by search request "${this.props.searchQuery}"`,
-              { theme: 'dark' }
-            );
-          }
-          this.setState(prevState => ({
-            searchResult: [...prevState.searchResult, ...result.hits],
-          }));
-        })
-        .catch(error => this.setState({ error }))
-        .finally(this.setState({ isLoading: false }));
-    }, 500);
-  };
-
-  onLoadMoreClick = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   closeModal = () => {
@@ -80,28 +24,25 @@ class ImageGallery extends Component {
   };
 
   render() {
-    const { searchResult, isLoading, showModal, largeImageURL, error } =
-      this.state;
+    const { showModal, largeImageURL, error } =
+        this.state;
+      const { searchResult } = this.props;
     return (
       <>
         {error && <h1>{error.message}</h1>}
-        {this.props.searchQuery && (
-          <ul className={css.imageGallery} onClick={this.clickHandler}>
-            {searchResult.map(({ id, webformatURL, tags, largeImageURL }) => (
-              <GalleryItem
-                key={id}
-                url={webformatURL}
-                title={tags}
-                largeImage={largeImageURL}
-              />
-            ))}
-          </ul>
-        )}
+        <ul className={css.imageGallery} onClick={this.clickHandler}>
+          {searchResult.map(({ id, webformatURL, tags, largeImageURL }) => (
+            <GalleryItem
+              key={id}
+              url={webformatURL}
+              title={tags}
+              largeImage={largeImageURL}
+            />
+          ))}
+        </ul>
         {showModal && (
           <Modal imageURL={largeImageURL} onClick={this.closeModal} />
         )}
-        {isLoading && <Loader />}
-        {searchResult.length > 0 && <Button onClick={this.onLoadMoreClick} />}
       </>
     );
   }
@@ -109,5 +50,5 @@ class ImageGallery extends Component {
 export default ImageGallery;
 
 ImageGallery.propTypes = {
-  searchQuery: PropTypes.string.isRequired,
+  searchResult: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
